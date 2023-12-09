f496683acf7b59425ba1df328e436a8208965b27
laurenyip <laurenyip10@gmail.com>
2023-12-09 10:25:58 -0800

    amend

---
+++ script/script.js
@@ -0,0 +1,75 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector(".navbar");
+
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 0) {
+    header.classList.add("navbarDark");
+  } else {
+    header.classList.remove("navbarDark");
+  }
+};
+
+// About Me Slideshow
+let aboutSlideIndex = 1;
+showAboutSlides(aboutSlideIndex);
+
+function plusAboutSlides(n) {
+  showAboutSlides((aboutSlideIndex += n));
+}
+
+function currentAboutSlide(n) {
+  showAboutSlides((aboutSlideIndex = n));
+}
+
+function showAboutSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    aboutSlideIndex = 1;
+  }
+  if (n < 1) {
+    aboutSlideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[aboutSlideIndex - 1].style.display = "block";
+  dots[aboutSlideIndex - 1].className += " active";
+}
+
+// ROSIE Slideshow
+let rosieSlideIndex = 1;
+showRosieSlides(rosieSlideIndex);
+
+function plusRosieSlides(n) {
+  showRosieSlides((rosieSlideIndex += n));
+}
+
+function currentRosieSlide(n) {
+  showRosieSlides((rosieSlideIndex = n));
+}
+
+function showRosieSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    rosieSlideIndex = 1;
+  }
+  if (n < 1) {
+    rosieSlideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[rosieSlideIndex - 1].style.display = "block";
+  dots[rosieSlideIndex - 1].className += " active";
+}
